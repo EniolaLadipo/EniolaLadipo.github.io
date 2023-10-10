30f2e238dcb396a8b2ee5e1d61d94cbabc8c1034
Eniola Ladipo <eniolaladipo15@gmail.com>
2023-10-10 14:06:28 +0100

    Update to contact page

---
+++ script.js
@@ -0,0 +1,6 @@
+function submitMessage(){
+    alert("Message has been sent!");
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+}
